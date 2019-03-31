6d11cfa51c495627872227112d1702de9f64becb
Stephane Vaillancourt <svailla4@gmail.com>
2019-03-31 13:45:54 -0400

    finished the trade micro service

--- handlers/trades.js
+++ handlers/trades.js
@@ -16,15 +16,19 @@ exports.fetchTrades = async function (request, h) {
 }
 
 
-exports.fetchMostTradedCompany = async function (request, h) {
+exports.deleteTrade = async function (request, h) {
     try {
-        const trades = await Trades.query();
+        const numDeleted = await Trades
+        .query()
+        .delete()
+        .where('id', '=', request.payload.id);
 
-        if (trades[0] instanceof Trades || trades.length ==0) {
-            return h.response(trades);
-        } else {
-            throw Boom.badRequest("Failed to fetch trades")
+        if(numDeleted===0){
+            throw Boom.badRequest("Nothing was deleted")
         }
+
+        return numDeleted
+
     } catch (err) {
         throw Boom.badRequest(err);
     }
--- routes/trades_routes.js
+++ routes/trades_routes.js
@@ -1,39 +1,14 @@
 const Trades = require('../handlers/trades')
-const Joi = require('joi');
 
 module.exports = [
     {
         method: 'GET',
         path: '/trades',
         handler: Trades.fetchTrades,
+    },
+    {
+        method: 'DELETE',
+        path: '/trades/delete',
+        handler: Trades.deleteTrade
     }
-    // {
-    //     method: 'POST',
-    //     path: '/login',
-    //     handler: Users.login,
-    //     options: {
-    //         validate: {
-    //             payload: {
-    //                 email: Joi.string().regex(regexUtils.email).required(), // email regex
-    //                 password: Joi.string().required()
-    //             }
-    //         }
-    //     }
-    // },
-    // {
-    //     method: 'POST',
-    //     path: '/logout',
-    //     handler: Users.logout,
-    //     options:{
-    //         auth: 'jwt'
-    //     }
-    // },
-    // {
-    //     method: 'GET',
-    //     path: '/verifySession',
-    //     handler: Users.verifySession,
-    //     options:{
-    //         auth:'jwt'
-    //     }
-    // }
 ]
