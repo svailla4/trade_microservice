7538a20a9668d18694c9f54399b2c3562cf8cf72
Eric Lammers <ericjameslammers@gmail.com>
2019-04-02 15:50:42 -0400

    Made composite key.

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,18 @@
 module.exports = {
 
   development: {
-      client: 'sqlite3',
+      client: 'postgresql',
       connection: {
-          filename: './dev.sqlite3'
+          database: 'trade_database',
+          user:     'admin',
+          password: 'password'
+      },
+      pool: {
+          min: 2,
+          max: 10
+      },
+      migrations: {
+          tableName: 'knex_migrations'
       }
   },
 
